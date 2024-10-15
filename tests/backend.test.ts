22dd4362fc14cd469e2f6ffa68f33f45d8c50c28
Andrew Morris <voltrevo@gmail.com>
2024-10-16 09:29:04 +1100

    add failing test

--- tests/backend.test.ts
+++ tests/backend.test.ts
@@ -1,7 +1,50 @@
 import { expect } from 'chai';
+import EmpCircuit from '../src/EmpCircuit';
 
-describe('eg', () => {
-  it('1 + 1 === 2', async () => {
-    expect(1 + 1).to.eq(2);
+describe('EmpCircuit', () => {
+  it('converts or into and,not', async () => {
+    const ec = new EmpCircuit(
+      {
+        bristol: `
+          1 3
+          2 1 1
+          1 1
+  
+          2 1 0 1 2 OR
+        `,
+        info: {
+          input_name_to_wire_index: {
+            a: 0,
+            b: 1,
+          },
+          constants: {},
+          output_name_to_wire_index: {
+            c: 2,
+          },
+        },
+      },
+      [
+        {
+          inputs: ['a'],
+          outputs: ['c'],
+        },
+        {
+          inputs: ['b'],
+          outputs: ['c'],
+        },
+      ],
+    );
+
+    const out = ec.getSimplifiedBristol();
+
+    expect(ec.getSimplifiedBristol()).to.equal([
+      '4 6',
+      '1 1 1',
+      '',
+      '1 1 0 2 INV',
+      '1 1 1 3 INV',
+      '2 1 2 3 4 AND',
+      '1 1 4 5 INV',
+    ].join('\n'));
   });
 });
