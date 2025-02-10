1a82cc6f8eebc7cce8e88c9524f4bfd3b4fb9cc8
Andrew Morris <voltrevo@gmail.com>
2025-02-10 11:58:31 +1100

    replace testOpt with test overload

--- tests/helpers/suite.ts
+++ tests/helpers/suite.ts
@@ -10,20 +10,25 @@ let suite: TestDefinition[] = [];
 let failures = 0;
 let autorun = true;
 
-export async function testOpt(
-  name: string,
-  options: TestDefinition['options'],
-  fn: () => unknown,
+export async function test(
+  ...args:
+    | [name: string, fn: () => unknown]
+    | [name: string, options: TestDefinition['options'], fn: () => unknown]
 ) {
+  let name, options, fn;
+
+  if (args.length === 2) {
+    [name, fn] = args;
+    options = {};
+  } else {
+    [name, options, fn] = args;
+  }
+
   suite.push({ name, options, fn });
 
   if (autorun) {
     queueMicrotask(runSuite);
   }
-}
-
-export async function test(name: string, fn: () => unknown) {
-  return testOpt(name, {}, fn);
 }
 
 export function setSuiteAutorun(value: boolean) {
--- tests/mpc.test.ts
+++ tests/mpc.test.ts
@@ -5,7 +5,7 @@ import * as summon from 'summon-ts';
 
 import { EmpWasmBackend } from '../src';
 
-import { test, testOpt } from './helpers/suite';
+import { test } from './helpers/suite';
 import AsyncQueueStore from './helpers/AsyncQueueStore';
 
 test("max(3, 5) === 5", async () => {
@@ -121,7 +121,7 @@ test("middle(8, 17, 5) == 8", async () => {
 // NOTE:  this circuit also failed with consensus on the same bad outputs before
 //        the N parties upgrade, so there's something else going on:
 //        https://github.com/voltrevo/emp-wasm-backend/commit/be67477
-testOpt("vickrey(8, 17, 5) == [1, 8]", { skip: true }, async () => {
+test("vickrey(8, 17, 5) == [1, 8]", { skip: true }, async () => {
   await summon.init();
 
   const circuit = summon.compileBoolean('/src/main.ts', 8, {
